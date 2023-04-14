c3291e28bb248e41370b24184f270d475ab1302a
MaxiBelli <maximiliano_belli@hotmail.com>
2023-04-14 11:40:51 -0300

    Add React Big Calendar

--- client/src/components/calendar/CalendarScreen.jsx
+++ client/src/components/calendar/CalendarScreen.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
-import { Navbar } from '../ui/Navbar';
+import React from "react";
+import { Navbar } from "../ui/Navbar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
-    <div><Navbar/></div>
-  )
-}
+    <div className="calendar-screen">
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        eventPropGetter={eventStyleGetter}
+      />
+    </div>
+  );
+};
